d258d514a32a6ed76c12bf971fcbbd276e93dfa4
cnelson20 <cjnelson2004@gmail.com>
2021-11-05 16:29:38 +0100

    main.c

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[]);
 int read_csv();
 struct pop_entry *read_data();
 int add_data();
+int update_data();
 
 int main(int argc, char *argv[]) {
   if (argc <= 1) {
@@ -33,12 +34,11 @@ int main(int argc, char *argv[]) {
   } else if (!strcmp("-add_data",argv[1])) {
 	return add_data();
   } else if (!strcmp("-update_data",argv[1])) {
-	void *p = read_data();
-	if (p != NULL) {
-		update_data(p);ak
-		return 0;
-	}
+	update_data();
 	return 1;
+  } else {
+	printf("Options: -read_csv -read_data -add_data -update_data\n");  
+	exit(-1);
   }
 }
 int read_csv() {
@@ -73,11 +73,6 @@ int read_csv() {
     int year;
     
 	int index = 0;
-	//while (index < strlen(pt)) {
-	//	printf("%c ",pt[1+index]);
-	//	index++;
-	//}
-	//printf("\n");
 	if (sscanf(pt+1,"%d,%d,%d,%d,%d,%d",&year,&(list[i].population),&(list[i+1].population),&(list[i+2].population),&(list[i+3].population),&(list[i+4].population)) < 6) {
 		//printf("sscanf error!\n");
 		break;
@@ -92,12 +87,9 @@ int read_csv() {
     i += 5;
   }
 
-  //printf("\n");
-  fd = open("nyc_data.bin",O_CREAT);
-  close(fd);
-  fd = open("nyc_data.bin",O_WRONLY);
+  fd = open("nyc_data.bin",O_WRONLY | O_CREAT | O_TRUNC);
   if (fd == -1) {printf("%s\n",strerror(errno)); return errno;}
-  write(fd,list,sizeof(struct pop_entry)*5*rows);
+  if (write(fd,list,sizeof(struct pop_entry)*5*rows) < sizeof(struct pop_entry)*5*rows) {printf("Write error: %s\n",strerror(errno));}
   close(fd);
   
   free(list);
@@ -170,7 +162,9 @@ int add_data() {
 	return 0;
 }
 
-int update_data(struct pop_entry *list) {
+int update_data() {
+	struct pop_entry *list = read_data();
+	
 	struct pop_entry entry;
 	int index, first_wrong_index;
 	char *borough;
@@ -198,10 +192,11 @@ int update_data(struct pop_entry *list) {
 		printf("\nPopulation: ");
 		fgets(input,256,stdin);
 	} while (sscanf(input,"%d\n",&(entry.population)) != 1);
+	// this sscanf doesnt like me
 	
-	printf("[%d] Year: %d Borough: %s Population: %d\n",index,entry.year,entry.boro,entry.population);
+	//printf("[%d] Year: %d Borough: %s Population: %d\n",index,entry.year,entry.boro,entry.population);
 	list[index] = entry;
-	printf("[%d] Year: %d Borough: %s Population: %d\n",index,list[index].year,list[index].boro,list[index].population);
+	//printf("[%d] Year: %d Borough: %s Population: %d\n",index,list[index].year,list[index].boro,list[index].population);
 	
 	int fd = open("nyc_data.bin",O_WRONLY);
 	if (fd == -1) {printf("%s\n",strerror(errno)); return errno;}
